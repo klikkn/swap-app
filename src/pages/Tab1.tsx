dad9a1bf0f417dedf85bc3bd052640dd26f6516a
Sergey Perezhogin <klikkn@Sergeys-MacBook-Pro.local>
2024-07-23 11:18:20 +0200

    feat: embla carousel is added

--- src/pages/Tab1.tsx
+++ src/pages/Tab1.tsx
@@ -1,8 +1,14 @@
 import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import useEmblaCarousel from 'embla-carousel-react'
+
 import ExploreContainer from '../components/ExploreContainer';
 import './Tab1.css';
 
 const Tab1: React.FC = () => {
+  const [emblaRef] = useEmblaCarousel({
+     axis: 'y'
+  })
+
   return (
     <IonPage>
       <IonHeader>
@@ -10,13 +16,22 @@ const Tab1: React.FC = () => {
           <IonTitle>Tab 1</IonTitle>
         </IonToolbar>
       </IonHeader>
-      <IonContent fullscreen>
+      <IonContent scrollY={false} scrollX={false}>
         <IonHeader collapse="condense">
           <IonToolbar>
             <IonTitle size="large">Tab 1</IonTitle>
           </IonToolbar>
         </IonHeader>
-        <ExploreContainer name="Tab 1 page" />
+
+
+        <div className="embla" ref={emblaRef}>
+          <div className="embla__container">
+            <div className="embla__slide">Slide 1</div>
+            <div className="embla__slide">Slide 2</div>
+            <div className="embla__slide">Slide 3</div>
+          </div>
+        </div>
+
       </IonContent>
     </IonPage>
   );
